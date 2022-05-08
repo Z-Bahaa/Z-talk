b650f870e8b32a3bdd45688b37b76397ab5653d4
Z-Bahaa <zizobahaapersonal@gmail.com>
2022-05-09 00:02:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'ea961f03-dbfd-4824-813f-064f410691cf';
+
+// the password is the same as the username Admin: {Z. Bahaa}
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
